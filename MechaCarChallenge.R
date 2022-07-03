d1d92a0466e6b2ac969269ea46592c1a7f0ebc1b
neilralston <100967106+neilralston@users.noreply.github.com>
2022-07-03 18:42:34 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,7 @@ library(tidyverse)
 Challenge1_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Challenge1_table) 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Challenge1_table))
+Challenge2_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- Challenge2_table %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance_PSI=var(PSI),Std_Dev_PSI=sd(PSI), .groups = 'keep')
+lot_summary <- Challenge2_table %>% group_by(Manufacturing_Lot) %>%summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance_PSI=var(PSI),Std_Dev_PSI=sd(PSI),Count=n(), .groups = 'keep')
